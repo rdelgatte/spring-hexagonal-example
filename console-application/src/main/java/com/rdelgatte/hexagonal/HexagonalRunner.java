8462dba146306ec0489a32b900312a5fb62c532c
Remi Delgatte <remi.delgatte@decathlon.com>
2019-03-06 19:33:40 +0100

    Step-04-solution: Adding business rule to prevent free products

--- console-application/src/main/java/com/rdelgatte/hexagonal/HexagonalRunner.java
+++ console-application/src/main/java/com/rdelgatte/hexagonal/HexagonalRunner.java
@@ -1,11 +1,12 @@
 package com.rdelgatte.hexagonal;
 
+import static java.util.UUID.randomUUID;
+
 import com.rdelgatte.hexagonal.api.CustomerService;
 import com.rdelgatte.hexagonal.api.ProductService;
-import com.rdelgatte.hexagonal.domain.Customer;
 import com.rdelgatte.hexagonal.domain.Product;
+import io.vavr.control.Try;
 import java.math.BigDecimal;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -26,22 +27,15 @@ public class HexagonalRunner implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
-    productService.getAllProducts()
+    createProduct(new Product(randomUUID(), "1", "Free product", BigDecimal.ZERO));
+    createProduct(new Product(randomUUID(), "2", "Product we give you money to buy it", BigDecimal.valueOf(-1)));
+    createProduct(new Product(randomUUID(), "3", "Real product", BigDecimal.valueOf(13.05)));
+  }
+
+  private void createProduct(Product product) {
+    Try.of(() -> productService.createProduct(product))
+        .onFailure(throwable -> log.error(throwable.getMessage()))
         .map(Product::toString)
-        .forEach(log::info);
-    String anyLogin = "rdelgatte";
-    customerService.signUp(anyLogin);
-    productService.createProduct(new Product(UUID.randomUUID(), "01123", "ANY_LABEL_0123", BigDecimal.valueOf(120.90)));
-    productService.createProduct(new Product(UUID.randomUUID(), "01456", "ANY_LABEL_0456", BigDecimal.valueOf(130.90)));
-    productService.createProduct(new Product(UUID.randomUUID(), "01789", "ANY_LABEL_0789", BigDecimal.valueOf(150.90)));
-    productService.getAllProducts()
-        .map(Product::toString)
-        .forEach(log::info);
-    customerService.addProductToCart(anyLogin, "01123");
-    customerService.addProductToCart(anyLogin, "01456");
-    customerService.addProductToCart(anyLogin, "01789");
-    customerService.findCustomer(anyLogin)
-        .map(Customer::toString)
-        .forEach(log::info);
+        .onSuccess(log::info);
   }
 }
