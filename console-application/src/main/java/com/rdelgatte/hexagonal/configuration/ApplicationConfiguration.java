2d62409e42bd7e819e6cb2d3afad86531d9fcc28
Remi Delgatte <remi.delgatte@decathlon.com>
2019-03-06 19:09:16 +0100

    Step-06: Configure mysql persistence

--- console-application/src/main/java/com/rdelgatte/hexagonal/configuration/ApplicationConfiguration.java
+++ console-application/src/main/java/com/rdelgatte/hexagonal/configuration/ApplicationConfiguration.java
@@ -5,31 +5,23 @@ import com.rdelgatte.hexagonal.api.CustomerServiceImpl;
 import com.rdelgatte.hexagonal.api.ProductService;
 import com.rdelgatte.hexagonal.api.ProductServiceImpl;
 import com.rdelgatte.hexagonal.infrastructure.memory.InMemoryCustomerRepository;
-import com.rdelgatte.hexagonal.infrastructure.postgres.repository.PostgresProductRepository;
-import com.rdelgatte.hexagonal.infrastructure.postgres.repository.PostgresSpringDataProductRepository;
+import com.rdelgatte.hexagonal.infrastructure.memory.InMemoryProductRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
 
-  private PostgresProductRepository postgresProductRepository(
-      PostgresSpringDataProductRepository postgresSpringDataProductRepository) {
-    return new PostgresProductRepository(postgresSpringDataProductRepository);
-  }
+  private InMemoryProductRepository inMemoryProductRepository = new InMemoryProductRepository();
+  private InMemoryCustomerRepository inMemoryCustomerRepository = new InMemoryCustomerRepository();
 
-  private InMemoryCustomerRepository getCustomerRepository() {
-    return new InMemoryCustomerRepository();
+  @Bean
+  CustomerService customerService() {
+    return new CustomerServiceImpl(inMemoryCustomerRepository, inMemoryProductRepository);
   }
 
   @Bean
-  CustomerService customerService(PostgresSpringDataProductRepository postgresSpringDataProductRepository) {
-    return new CustomerServiceImpl(getCustomerRepository(),
-        postgresProductRepository(postgresSpringDataProductRepository));
-  }
-
-  @Bean
-  ProductService productService(PostgresSpringDataProductRepository postgresSpringDataProductRepository) {
-    return new ProductServiceImpl(postgresProductRepository(postgresSpringDataProductRepository));
+  ProductService productService() {
+    return new ProductServiceImpl(inMemoryProductRepository);
   }
 }
