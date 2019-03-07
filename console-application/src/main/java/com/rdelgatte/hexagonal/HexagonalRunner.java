5037af84cfcff256226152701f2357d05b713e95
Remi Delgatte <remi.delgatte@decathlon.com>
2019-03-07 09:17:56 +0100

    Step-03: Implement console application

--- console-application/src/main/java/com/rdelgatte/hexagonal/HexagonalRunner.java
+++ console-application/src/main/java/com/rdelgatte/hexagonal/HexagonalRunner.java
@@ -1,11 +1,12 @@
 package com.rdelgatte.hexagonal;
 
+import static io.vavr.API.Set;
 import static java.util.UUID.randomUUID;
 
 import com.rdelgatte.hexagonal.api.CustomerService;
 import com.rdelgatte.hexagonal.api.ProductService;
 import com.rdelgatte.hexagonal.domain.Product;
-import io.vavr.control.Try;
+import io.vavr.collection.Set;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,15 +28,12 @@ public class HexagonalRunner implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
-    createProduct(new Product(randomUUID(), "1", "Free product", BigDecimal.ZERO));
-    createProduct(new Product(randomUUID(), "2", "Product we give you money to buy it", BigDecimal.valueOf(-1)));
-    createProduct(new Product(randomUUID(), "3", "Real product", BigDecimal.valueOf(13.05)));
-  }
-
-  private void createProduct(Product product) {
-    Try.of(() -> productService.createProduct(product))
-        .onFailure(throwable -> log.error(throwable.getMessage()))
+    Set<Product> products = Set(new Product(randomUUID(), "1", "Free product", BigDecimal.ZERO),
+        new Product(randomUUID(), "2", "Product we give you money to buy it", BigDecimal.valueOf(-1)),
+        new Product(randomUUID(), "3", "Real product", BigDecimal.valueOf(13.05)));
+    products.map(productService::createProduct);
+    productService.getAllProducts()
         .map(Product::toString)
-        .onSuccess(log::info);
+        .forEach(log::info);
   }
 }
