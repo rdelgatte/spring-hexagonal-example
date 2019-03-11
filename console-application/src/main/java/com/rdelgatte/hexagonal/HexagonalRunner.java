126b09cbe17fc7e93f06b7e350c7e1f4f702d8a3
Rémi Delgatte <remi.delgatte@gmail.com>
2019-03-11 08:52:22 +0100

    Feat(console): improve console messages (#7)

--- console-application/src/main/java/com/rdelgatte/hexagonal/HexagonalRunner.java
+++ console-application/src/main/java/com/rdelgatte/hexagonal/HexagonalRunner.java
@@ -1,11 +1,14 @@
 package com.rdelgatte.hexagonal;
 
+import static java.util.UUID.randomUUID;
+
 import com.rdelgatte.hexagonal.api.CustomerService;
 import com.rdelgatte.hexagonal.api.ProductService;
 import com.rdelgatte.hexagonal.domain.Customer;
 import com.rdelgatte.hexagonal.domain.Product;
+import io.vavr.CheckedFunction0;
+import io.vavr.control.Try;
 import java.math.BigDecimal;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -26,22 +29,32 @@ public class HexagonalRunner implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
-    productService.getAllProducts()
+    final Product product1 = new Product(randomUUID(), "1", "Free product", BigDecimal.valueOf(10.95));
+    final Product product2 = new Product(randomUUID(), "2", "Product we give you money to buy it",
+        BigDecimal.valueOf(20.50));
+    final Product product3 = new Product(randomUUID(), "3", "Real product", BigDecimal.valueOf(13.05));
+    productAction(() -> productService.createProduct(product1));
+    productAction(() -> productService.createProduct(product2));
+    productAction(() -> productService.createProduct(product3));
+
+    String customerName = "Rémi";
+    customerAction(() -> customerService.signUp(customerName));
+    customerAction(() -> customerService.addProductToCart(customerName, "1"));
+    customerAction(() -> customerService.addProductToCart(customerName, "2"));
+    customerAction(() -> customerService.addProductToCart(customerName, "3"));
+  }
+
+  private void productAction(CheckedFunction0<Product> productFunction) {
+    Try.of(productFunction)
+        .onFailure(throwable -> log.error(throwable.getMessage()))
         .map(Product::toString)
-        .forEach(log::info);
-    String anyLogin = "rdelgatte";
-    customerService.signUp(anyLogin);
-    productService.createProduct(new Product(UUID.randomUUID(), "01123", "ANY_LABEL_0123", BigDecimal.valueOf(120.90)));
-    productService.createProduct(new Product(UUID.randomUUID(), "01456", "ANY_LABEL_0456", BigDecimal.valueOf(130.90)));
-    productService.createProduct(new Product(UUID.randomUUID(), "01789", "ANY_LABEL_0789", BigDecimal.valueOf(150.90)));
-    productService.getAllProducts()
-        .map(Product::toString)
-        .forEach(log::info);
-    customerService.addProductToCart(anyLogin, "01123");
-    customerService.addProductToCart(anyLogin, "01456");
-    customerService.addProductToCart(anyLogin, "01789");
-    customerService.findCustomer(anyLogin)
+        .onSuccess(log::info);
+  }
+
+  private void customerAction(CheckedFunction0<Customer> customerFunction) {
+    Try.of(customerFunction)
+        .onFailure(throwable -> log.error(throwable.getMessage()))
         .map(Customer::toString)
-        .forEach(log::info);
+        .onSuccess(log::info);
   }
 }
